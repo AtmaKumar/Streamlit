a4c8f37035972ccc09fb2c81bc21c22d3360dfe4
atma kumar Rai <58214761+AtmaKumar@users.noreply.github.com>
2022-08-28 13:39:04 +0530

    streamlit text_input

---
+++ learn.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+# st.title("Learning Streamlit")
+
+# st.header("Learning Streamlit")
+# st.markdown("Learning Streamlit")
+
+# st.caption("Learning Streamlit")
+
+
+# x = '''for i in range(1,4):
+#      print("Hello, Streamlit!")'''
+# st.code(x)
+
+
+st.latex("Learning Streamlit")
+
+
+
+
